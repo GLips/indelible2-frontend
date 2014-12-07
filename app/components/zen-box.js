5b67bd46ca313aad79d0560bf333bd31c299d3ad
GLips <glipsman@gmail.com>
2014-12-06 19:39:11 -0700

    Add a characters per minute counter to the Zenbox and its template.

--- app/components/zen-box.js
+++ app/components/zen-box.js
@@ -1,6 +1,12 @@
 import Ember from "ember";
 
 export default Ember.Component.extend({
+  content: "",
+  charactersPerMinute: function() {
+    var content = this.get('content'),
+        timeElapsed = this.get('clock.pulse');
+    return (content.length / timeElapsed).toFixed(2);
+  }.property('content', 'clock.pulse'),
   timeElapsed: function() {
     return this.get('clock.pulse');
   }.property('clock.pulse')
