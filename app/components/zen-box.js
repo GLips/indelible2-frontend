80d510067496129c89bf3b09f13708737dc8d224
GLips <glipsman@gmail.com>
2014-12-10 22:50:27 -0700

    Start tracking characters per minute when the user starts typing instead of at page load.

--- app/components/zen-box.js
+++ app/components/zen-box.js
@@ -2,12 +2,18 @@ import Ember from "ember";
 
 export default Ember.Component.extend({
   content: "",
+  startTime: 0,
   charactersPerMinute: function() {
     var content = this.get('content'),
-        timeElapsed = this.get('clock.pulse');
+        timeElapsed = this.get('timeElapsed');
+
+    if(this.get('startTime') === 0 && content.length > 0) {
+      this.set('startTime', timeElapsed);
+    }
+
     return (content.length / timeElapsed).toFixed(2);
   }.property('content', 'clock.pulse'),
   timeElapsed: function() {
-    return this.get('clock.pulse');
+    return this.get('clock.pulse') - this.get('startTime');
   }.property('clock.pulse')
 });
