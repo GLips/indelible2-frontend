461d78b94e52afad7c6539e8ae33b30b3d8dc59f
GLips <glipsman@gmail.com>
2015-02-22 12:31:52 -0700

    Include Bootstrap glyphicon fonts in the application.

--- Brocfile.js
+++ Brocfile.js
@@ -8,17 +8,10 @@ app.import('vendor/crypto/core-min.js')
 app.import('vendor/crypto/aes.js')
 app.import('vendor/crypto/sha3.js')
 
-// Use `app.import` to add additional libraries to the generated
-// output files.
-//
-// If you need to use different assets in different
-// environments, specify an object as the first parameter. That
-// object's keys should be the environment name and the values
-// should be the asset to use in that environment.
-//
-// If the library that you are including contains AMD or ES6
-// modules that you would like to import into your application
-// please specify an object with the list of modules as keys
-// along with the exports of each module as its value.
+var pickFiles = require('broccoli-static-compiler');
+var bootstrapFonts = pickFiles('bower_components/bootstrap-sass/assets/fonts/bootstrap', {
+    srcDir: '/',
+    destDir: '/fonts/bootstrap'
+});
 
-module.exports = app.toTree();
+module.exports = app.toTree(bootstrapFonts);
