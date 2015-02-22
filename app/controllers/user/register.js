ccf69935682bab4075df5d3459991f1974f3039e
GLips <glipsman@gmail.com>
2015-02-21 22:02:06 -0700

    Users are logged in automatically after a successful registration.

--- app/controllers/user/register.js
+++ app/controllers/user/register.js
@@ -3,8 +3,12 @@ import Ember from "ember";
 export default Ember.ObjectController.extend({
   actions: {
     register: function() {
-      localStorage.setItem("password", this.get('model.password'));
-      this.get('model').save();
+      var password = this.get('model.password'),
+          _this = this;
+      this.get('model').save().then(function(data) {
+        _this.get('session').login(data, password);
+        _this.transitionToRoute('entries');
+      }, function() { /* Catch errors */ });
     }
   }
 });
