3918fbc954f7b543bd6013da8ceabc6daa99f3a9
GLips <glipsman@gmail.com>
2015-02-22 20:37:22 -0700

    Redirect to entries index on creation of a new entry.

--- app/controllers/entries/new.js
+++ app/controllers/entries/new.js
@@ -3,7 +3,9 @@ import Ember from "ember";
 export default Ember.ObjectController.extend({
   actions: {
     saveEntry: function() {
-      this.get('model').save();
+      this.get('model').save().then(()=> {
+        this.transitionToRoute('entries');
+      }, function() { /* Catch errors */ });
     }
   }
 });
